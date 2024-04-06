ea973336f87b2fbca92a94c5be99622f0c27cd28
MartinhoGon <martinhogon@gmail.com>
2024-04-06 22:58:13 +0000

    Change in the folder structure

--- backend/api/views/dataView.py
+++
@@ -1,8 +0,0 @@
-from rest_framework.response import Response
-from rest_framework import status
-from rest_framework.decorators import api_view
-from django.http import JsonResponse
-from .models/data import Data
-
-@api_view(['GET'])
-def getDataList(request):
