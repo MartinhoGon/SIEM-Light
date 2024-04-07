31c6ab86a6f95a982572fe2bee6206ddcded154d
MartinhoGon <martinhogon@gmail.com>
2024-04-07 21:16:31 +0000

    Bug in urls.py, was missing the api/ urls

--- backend/siemapi/api/admin.py
+++ backend/siemapi/api/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from api.models import *
+# from api.models import *
 # Register your models here.
 
-admin.site.register(feed.Feed)
-admin.site.register(category.Category)
-admin.site.register(value.Value)
+# admin.site.register(feed.Feed)
+# admin.site.register(category.Category)
+# admin.site.register(value.Value)
 
 
