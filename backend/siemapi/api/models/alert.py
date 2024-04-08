d57d686685e39b6d487e09495408b989b496d54c
MartinhoGon <martinhogon@gmail.com>
2024-04-08 21:13:13 +0000

    Changed Enum from alert.py

--- backend/siemapi/api/models/alert.py
+++ backend/siemapi/api/models/alert.py
@@ -1,15 +1,15 @@
 from django.db import models
 from enum import Enum
 
-class ValueType(Enum):
+class AlertLevel(Enum):
     LOW = 'Low'
     MEDIUM = 'Medium'
     HIGH = 'High'
     CRITICAL = 'Critical'
 
 class Alert(models.Model):
-    level = models.CharField(max_length=20, choices=[(tag.value, tag.name) for tag in ValueType])
+    level = models.CharField(max_length=20, choices=[(tag.value, tag.name) for tag in AlertLevel])
     message = models.TextField()
-    
+
     def __str__(self):
         return self.name
