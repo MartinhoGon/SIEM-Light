130b4e70e39aa9bb64c66da2899c9a8f0b3a4100
MartinhoGon <martinhogon@gmail.com>
2024-04-25 20:34:16 +0000

    Changed DB engine from MySql to PostgreSQL. Also added project fetcheer that will be responsible for handdling all Feeds

--- backend/siemapi/api/models/alert.py
+++ backend/siemapi/api/models/alert.py
@@ -10,6 +10,7 @@ class AlertLevel(Enum):
 class Alert(models.Model):
     level = models.CharField(max_length=20, choices=[(tag.value, tag.name) for tag in AlertLevel])
     message = models.TextField()
+    acknowledge = models.BooleanField(default=False)
 
     def __str__(self):
         return self.name
--- backend/siemapi/api/models/feed.py
+++ backend/siemapi/api/models/feed.py
@@ -14,7 +14,7 @@ class Feed(models.Model):
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=255)
     url = models.URLField()
-    type = models.CharField(max_length=10, choices=TYPE_CHOICES)
+    parser = models.CharField(max_length=10, choices=TYPE_CHOICES)
     refresh = models.DurationField()
 
     def __str__(self):
