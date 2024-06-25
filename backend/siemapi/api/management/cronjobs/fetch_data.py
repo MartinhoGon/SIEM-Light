4e03830a19f073896fd2b89fdf018e94185233a6
MartinhoGon <martinhogon@gmail.com>
2024-06-25 13:57:05 +0000

    Altered cronjob that fetches data from feeds. Know it deletes all entries in the database that have the checkValue = 0 after fetching and parse all data.

--- backend/siemapi/api/management/cronjobs/fetch_data.py
+++ backend/siemapi/api/management/cronjobs/fetch_data.py
@@ -1,19 +1,21 @@
 from api.models import Feed, Value
 from api.utils import DataFetcher, Parser
 from datetime import datetime
+from api.logger import get_logger
 
 def get_feed():
+    logger = get_logger()
     #Starting by decreasing all values by 1:
     Value.decrementCheckValue()
     current_datetime = datetime.now()
     formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-    print("{} -  Starting fetching data from feeds".format(formatted_datetime))
+    logger.info("{} -  Starting fetching data from feeds".format(formatted_datetime))
     feeds = Feed.objects.all()
     for feed in feeds:
         data = DataFetcher.fetch(feed)
         current_datetime = datetime.now()
         formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-        print("{} -  Getting feed {}".format(formatted_datetime, feed.name))
+        logger.info("{} -  Getting feed {}".format(formatted_datetime, feed.name))
         if data:
             if feed.parser == 'txt':
                 data = Parser.parseText(data)
@@ -30,8 +32,10 @@ def get_feed():
             pass
             #self.stdout.write(self.style.ERROR(f'Failed to fetch data from feed: {feed.url}'))
         # break
+    deleted_count, _ = Value.objects.filter(checkValue=0).delete()
+    logger.info("Deleted {} values that had the checkValue equal to zero. This values were not found in any of the collected feeds.".format(deleted_count))
     current_datetime = datetime.now()
     formatted_datetime = current_datetime.strftime("%Y-%m-%d %H:%M:%S")
-    print("{} -  Ending fetching data from feeds".format(formatted_datetime))
+    logger.info("{} -  Ending fetching data from feeds".format(formatted_datetime))
 
 
